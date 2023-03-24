530629c47d0e3e3366190bbfb798733e82a7d59f
merimartirosyan246 <lusine.aghajanyan77@mail.ru>
2023-03-24 15:41:02 +0400

    first commit

---
+++ Jv6.js
@@ -0,0 +1,60 @@
+btn.onclick=function(){
+if(door.style.transform=='rotateY(58deg)'){
+    door.style.transform='rotate(0deg)'
+    door.style.transformOrigin='100% 0'
+}
+else{
+    door.style.transform='rotateY(58deg)'
+    door.style.transformOrigin='100% 0'
+}
+}
+door1.onclick=function(){
+    if(door1.style.transform=='rotateY(58deg)'){
+        door1.style.transform='rotate(0deg)'
+        door1.style.transformOrigin='100% 0'
+    }
+    else{
+        door1.style.transform='rotateY(58deg)'
+        door1.style.transformOrigin='100% 0'
+    }
+    }
+    door2.onclick=function(){
+        if(door2.style.transform=='rotateY(58deg)'){
+            door2.style.transform='rotate(0deg)'
+            door2.style.transformOrigin='100% 0'
+        }
+        else{
+            door2.style.transform='rotateY(58deg)'
+            door2.style.transformOrigin='100% 0'
+        }
+        }
+        door3.onclick=function(){
+            if(door3.style.transform=='rotateY(58deg)'){
+                door3.style.transform='rotate(0deg)'
+                door3.style.transformOrigin='100% 0'
+            }
+            else{
+                door3.style.transform='rotateY(58deg)'
+                door3.style.transformOrigin='100% 0'
+            }
+            }
+            door4.onclick=function(){
+            if(door4.style.transform=='rotateY(58deg)'){
+                door4.style.transform='rotate(0deg)'
+                door4.style.transformOrigin='100% 0'
+            }
+            else{
+                door4.style.transform='rotateY(58deg)'
+                door4.style.transformOrigin='100% 0'
+            }
+        }
+        door5.onclick=function(){
+            if(door5.style.transform=='rotateY(58deg)'){
+                door5.style.transform='rotate(0deg)'
+                door5.style.transformOrigin='100% 0'
+            }
+            else{
+                door5.style.transform='rotateY(58deg)'
+                door5.style.transformOrigin='100% 0'
+            }
+        }
